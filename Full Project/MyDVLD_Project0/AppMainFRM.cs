f4d1cf8fbb2cfde3282acb803e4e765dafe4c46d
Tony <tony.guirguis.dell@gmail.com>
2026-03-10 19:12:13 -0400

    Full Project Rebo

---
+++ Full Project/MyDVLD_Project0/AppMainFRM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyDVLD_Project0
+{
+    public partial class AppMainFRM : Form
+    {
+        public AppMainFRM()
+        {
+            InitializeComponent();
+        }
+
+        private void AppMainFRM_Load(object sender, EventArgs e)
+        {
+            this.BackgroundImage = Image.FromFile(@"C:\Users\Tonyg\OneDrive\Desktop\Desktop Projects_LocalRebo\DVLD Project\Project Icons\App Icons\Icons\Icons\WinForms MDI Background Teal Details.png");
+            this.BackgroundImageLayout = ImageLayout.Zoom;
+        }
+    }
+}
